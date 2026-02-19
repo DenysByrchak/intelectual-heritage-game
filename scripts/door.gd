4598a0b5f4c6c3f3f11f3d8a26f40c17df2b8ae7
Denys <denysbyrchak@gmail.com>
2026-02-18 21:35:54 -0500

    added simple door that moves to next level

---
+++ scripts/door.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@export var next_level: PackedScene
+
+var bodies: int = 0
+
+func _on_door_area_body_exited(body: Node2D) -> void:
+	bodies -= 1
+	timer.stop()
+	
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_packed(next_level)
+
+
+func _on_door_area_body_entered(body: Node2D) -> void:
+	bodies += 1
+	if bodies == 2:
+		timer.start()
