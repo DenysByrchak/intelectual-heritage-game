33412c347f3f4a362a3b0a8a140906a1d1ba21a5
Denys <denysbyrchak@gmail.com>
2026-02-21 20:46:30 -0500

    Added trap door, labels, and dialogue addon

---
+++ scripts/trap_door.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@export var enemy: PackedScene
+
+@onready var collision_shape_2d: CollisionShape2D = $DoorArea/CollisionShape2D
+
+var bodies: int = 0
+
+func _on_door_area_body_exited(body: Node2D) -> void:
+	bodies -= 1
+	timer.stop()
+	
+
+
+func _on_timer_timeout() -> void:
+	var enemy_amount = randi_range(1,3)
+	var enemy_offset : int = 0
+	
+	for i in range(enemy_amount):
+		enemy_offset = randi_range(-100, 100)
+		
+		var new_enemy = enemy.instantiate()
+		new_enemy.position.x += enemy_offset
+		new_enemy.position.y -= 100
+		add_child(new_enemy)
+		
+	collision_shape_2d.disabled = true
+	bodies = 0
+
+func _on_door_area_body_entered(body: Node2D) -> void:
+	bodies += 1
+	if bodies == 2:
+		timer.start()
