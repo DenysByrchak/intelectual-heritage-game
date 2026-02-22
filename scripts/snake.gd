565ead898ac28d89ace8a3a40263da340b848024
Denys <denysbyrchak@gmail.com>
2026-02-22 13:35:13 -0500

    removed rays from snake

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 class_name Snake
 
-@onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
