db8b2cf6464458d1654a52d505346657e79c87bc
Denys <denysbyrchak@gmail.com>
2026-03-12 10:37:56 -0400

    added dialogue and score UI

--- scripts/score_label.gd
+++ scripts/score_label.gd
@@ -1,9 +1,11 @@
 extends Label
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
 
 func _ready():
 	# Set starting text
 	text = "Score: " + str(ScoreManager.score)
 	position = Vector2(20, 20)
+	sprite_2d.position = position + Vector2(48, 11)
 	# Connect to score signal
 	ScoreManager.score_changed.connect(_on_score_changed)
 
